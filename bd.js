5867dfe3b75c1bd743735dc193a8dd495db0d059
Gabriel Leo Beltrao <129813388+Gabrielleobeltrao@users.noreply.github.com>
2026-03-02 19:44:44 +0000

    Fake database structure

---
+++ bd.js
@@ -0,0 +1,20 @@
+const categories = [
+    {
+        id: 1,
+        name: "teste",
+        type: "teste de tipo",
+        description: "teste de descricao",
+    }
+]
+
+const transactions = [
+    {
+        id: 1,
+        date: "2026-03-02",
+        amount: 1000.00,
+        description: "descricao da transacao",
+        category: "",
+    }
+]
+
+export default {categories, transactions}
