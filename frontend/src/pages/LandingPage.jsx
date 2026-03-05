db6de552f588d1492fcd2f381846d4765e02cf8c
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2026-03-05 15:26:23 -0500

    Configure BrowserRouter

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,7 @@
+function LandingPage() {
+    return (
+        <h1 className="text-3xl font-bold">Landing Page</h1>
+    )
+}
+
+export default LandingPage
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+function Login() {
+    return (
+        <h1 className="text-2xl font-bold">Login Page</h1>
+    )
+}
+
+export default Login
