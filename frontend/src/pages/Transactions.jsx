bf3462700464b91e1333972cd36cd9ee68157442
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2026-03-06 17:59:09 -0500

    Adjusting text size

--- frontend/src/pages/Transactions.jsx
+++ frontend/src/pages/Transactions.jsx
@@ -175,7 +175,7 @@ function Transactions() {
             <div className="h-full min-h-0 w-full col-span-6 p-4 border-r-4 border-gray-200 flex flex-col">
                 <div className="flex items-center justify-between">
                     <h2 className="text-lg font-bold">Transactions</h2>
-                    <button className="text-base font-bold text-white bg-gray-400 rounded-md px-4 py-2">
+                    <button className="text-sm font-bold text-white bg-gray-400 rounded-md px-4 py-2">
                         Upload Transactions
                     </button>
                 </div>
@@ -192,7 +192,7 @@ function Transactions() {
             <div className="h-full w-full col-span-2 p-4 flex flex-col gap-6">
                 <div className="flex items-center justify-between">
                     <h2 className="text-lg font-bold">Categories</h2>
-                    <button className="text-base font-bold text-white bg-gray-400 rounded-md px-4 py-2">
+                    <button className="text-sm font-bold text-white bg-gray-400 rounded-md px-4 py-2">
                         New Category
                     </button>
                 </div>
