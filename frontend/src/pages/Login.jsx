70f0e85cb0ee93547af02234cb99960427acefa1
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2026-03-06 21:33:39 -0500

    Login page setup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,24 @@
 function Login() {
     return (
-        <h1 className="text-2xl font-bold">Login Page</h1>
+        <section className="w-full h-dvh flex items-center justify-center">
+            <div className="flex flex-col gap-4 w-1/3">
+            <input
+                className="border-2 border-gray-100 rounded-full px-3 py-2 placeholder:text-black"
+                type="text"
+                placeholder="Email"
+            />
+            <input
+                className="border-2 border-gray-100 rounded-full px-3 py-2 placeholder:text-black"
+                type="password"
+                placeholder="Password"
+            />
+            <button 
+                className="bg-gray-100 rounded-full p-2"
+            >
+                Login
+            </button>
+            </div>
+        </section>
     )
 }
-
 export default Login
