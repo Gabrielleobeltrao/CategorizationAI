09cd98956842f636705824a6322fe0bb6a407fc2
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2026-03-10 17:51:57 -0400

    Add repositories for category

---
+++ backend/src/repositories/category.repository.js
@@ -0,0 +1,44 @@
+import { getDB } from "../db"
+
+// criar
+
+export async function createCategory(input) {
+    const db = getDB()
+
+    // adicionar informacoes da categoria
+    const doc = {
+        name: input.name
+    }
+
+    const result = await db.collection("categories").insertOne(doc)
+    return { ...doc, _id: result.insertedId }
+}
+
+// atualizar 
+
+export async function updateOfficeById(id, patch) {
+    const db = getDB()
+
+    // informacoes que podem ser alteradas
+    const { name } = patch
+
+    return db.collection({ categories }).findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: { name, updatedAt: new Date() } },
+        { returnDocument: "after" }
+    )
+}
+
+// buscar lista
+
+export async function listCategoriesByClientId(clientId) {
+    const db = getDB()
+    return db.collection("categories").find({ clientId }).sort({ createdAt: -1}).toArray()
+}
+
+// buscar categoria 
+
+export async function getCategoryById(id) {
+    const db = getDB()
+    return db.collection("categories").findOne({_id: new ObjectId(id)})
+}
