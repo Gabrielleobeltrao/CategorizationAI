6253683f7314847d8b8bf00ff24f049445518556
Gabriel Leo Beltrao <gabrielleoaus@gmail.com>
2026-03-10 21:46:44 -0400

    Initialize transactions indexes during server startup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,14 @@ import "dotenv/config"
 import { connectDB, getDB } from "./db.js"
 import app from "./app.js"
 import { createAuth } from "./lib/auth.js"
+import { ensureTransactionsIndexes } from "./repositories/transactions.repository.js"
 
 const PORT = process.env.PORT || 3001
 
 await connectDB()
+
+await ensureTransactionsIndexes()
+
 app.locals.auth = createAuth(getDB())
 
 app.listen(PORT, () => console.log(`API running on ${PORT}`))
